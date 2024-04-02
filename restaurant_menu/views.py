13ee7f47384032cb916b09c828e206d78ce04237
cmolarti <cmolarti@gmail.com>
2024-04-01 23:57:25 -0400

    created jinja syntax in index.html with for loops and if condition

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Item        #from models.py
+from .models import MEAL_TYPE
 
 
 class MenuList(generic.ListView):   #this function requires very name specific variable
     queryset = Item.objects.order_by('date_created')
     template_name = 'index.html'
 
-    def get_context_data(self): #this is specific named for django
-        context = {'meals':'Pizza',
-                   'ingredients':['Pepperoni', 'Tomato', 'Mozzarella']}
+    def get_context_data(self, **kwargs): #this is specific named for django
+        context = super().get_context_data(**kwargs)    #this will pull the data from the database
+        context['meals'] = MEAL_TYPE
+
         return context
 
 class MenuItemDetail(generic.DetailView):
