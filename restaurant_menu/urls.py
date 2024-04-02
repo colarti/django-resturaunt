559230125729fa5bae9ecd746b6e2937220d0439
cmolarti <cmolarti@gmail.com>
2024-04-02 18:57:04 -0400

    included the MenuItem for each meal, that will create a new webpage with the description information

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home')    #when the visitor the homes page, then get all information from index.html3=
+    path('', views.MenuList.as_view(), name='home'),    #when the visitor the homes page, then get all information from index.html3=
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name='menu_item')
 ]
