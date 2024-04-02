ecd2256d6ea9234e52f2d759b3379d6e41262228
cmolarti <cmolarti@gmail.com>
2024-04-01 23:29:03 -0400

    created an admin interface by updating restaurant_menu/admin.py with specific variable names that are assigned to tuples within models.py

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'status')   #the variables are coming from models.py in class Item
+    list_filter = ('status',)
+    search_fields = ('meal', 'description')
+
+
+admin.site.register(Item, MenuItemAdmin)    #register couples the models.Item with this MenuItemAdmin
+
+
